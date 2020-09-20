f9a8d04a876f985474117e79f48a7d97bbb3d659
Evan Summers <evan.summers@gmail.com>
2020-09-20 17:40:44 +0200

    Cosmetic code changes

--- examples/redis-bcrypt/publish.js
+++ examples/redis-bcrypt/publish.js
@@ -1,47 +1,49 @@
 const assert = require('assert')
 
-module.exports = ({ config, logger, redisClient }) => {
-  const { keyPrefix } = config.redis
-  return async (pub, client) => {
-    if (client) {
-      const { topic, payloadString, payloadObject } = pub
-      assert(payloadObject, 'payloadObject')
-      const type = payloadObject.type || 'none'
-      const clientId = client.id
-      logger.debug({ clientId, topic, payloadString }, 'publish')
-      await redisClient
-        .multi([
-          ['publish', `${keyPrefix}published:p`, payloadString],
-          [
-            'publish',
-            `${keyPrefix}published:topic:${topic.replace(/:/g, '')}:p`,
-            payloadString,
-          ],
-          [
-            'publish',
-            `${keyPrefix}published:source:${clientId.replace(/:/g, '')}:p`,
-            payloadString,
-          ],
-          ['zincrby', 'published:z', 1, [clientId, type].join(':')],
-          ['zincrby', 'published:type:z', 1, type],
-          ['zincrby', 'published:source:z', 1, clientId],
-          [
-            'xadd',
-            `published:x`,
-            'maxlen',
-            config.maxlen,
-            '*',
-            'source',
-            clientId,
-            'topic',
-            topic,
-            'payload',
-            payloadString,
-            'type',
-            type,
-          ],
-        ])
-        .exec()
-    }
+module.exports = ({ config, logger, redisClient }) => async (pub, client) => {
+  if (!client) {
+    logger.debug('no client')
+    return
   }
+  const { topic, payloadString, payloadObject } = pub
+  assert(payloadObject, 'payloadObject')
+  const type = payloadObject.type || 'none'
+  const clientId = client.id
+  logger.debug({ clientId, topic, payloadString }, 'publish')
+  await redisClient
+    .multi([
+      ['publish', `${config.redis.keyPrefix}published:p`, payloadString],
+      [
+        'publish',
+        `${config.redis.keyPrefix}published:topic:${topic.replace(/:/g, '')}:p`,
+        payloadString,
+      ],
+      [
+        'publish',
+        `${config.redis.keyPrefix}published:source:${clientId.replace(
+          /:/g,
+          '',
+        )}:p`,
+        payloadString,
+      ],
+      ['zincrby', 'published:z', 1, [clientId, type].join(':')],
+      ['zincrby', 'published:type:z', 1, type],
+      ['zincrby', 'published:source:z', 1, clientId],
+      [
+        'xadd',
+        `published:x`,
+        'maxlen',
+        config.maxlen,
+        '*',
+        'source',
+        clientId,
+        'topic',
+        topic,
+        'payload',
+        payloadString,
+        'type',
+        type,
+      ],
+    ])
+    .exec()
 }
